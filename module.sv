4083f1d52fd06d72a0826748272d59b26b0ec679
Rafal Kolucki <rkolucki@antmicro.com>
2023-03-28 13:29:20 +0200

    Add test (broken) module
    Signed-off-by: Rafal Kolucki <rkolucki@antmicro.com>

---
+++ module.sv
@@ -0,0 +1,9 @@
+
+module hello(input clk);
+
+initial
+begin: proc_he
+			        $display("hello 1");                                                                                           
+		$display("hello2");			                                                                             
+
+endmodule
